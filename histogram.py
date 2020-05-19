5c0675cd0e49f1ce8478a85138bcf63ce837fd16
cchicote <cchicote@student.42.fr>
2020-05-19 18:23:38 +0200

    Histogram done

--- histogram.py
+++ histogram.py
@@ -5,30 +5,7 @@ import describe
 
 classes = ['Arithmancy', 'Astronomy', 'Herbology', 'Defense Against the Dark Arts', 'Divination', 'Muggle Studies', 'Ancient Runes', 'History of Magic', 'Transfiguration', 'Potions', 'Care of Magical Creatures', 'Charms', 'Flying']
 houses = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
-
-'''
-def prepare_dataset(dataset):
-    # The results data will have this format:
-    # {'House': {'Class1': [grades], 'Class2': [grades]}}
-    results = {}
-    results['Gryffindor'] = {}
-    results['Slytherin'] = {}
-    results['Hufflepuff'] = {}
-    results['Ravenclaw'] = {}
-    list_size = int(dataset['Index'][len(dataset['Index']) - 1])
-    for i in range(list_size + 1):
-        house = ''
-        for key in dataset.keys():
-            if key == 'Hogwarts House':
-                house = dataset[key][i]
-            if key in classes:
-                try:
-                    grade = describe.try_float(dataset[key][i])
-                    results[house][key].append(grade)
-                except KeyError:
-                    results[house][key] = []
-                    results[house][key].append(grade)
-'''
+houses_colors = {'Gryffindor': '#7F0909', 'Slytherin': '#0D6217', 'Hufflepuff': '#EEE117', 'Ravenclaw': '#000A90'}
 
 def get_min(values):
     return sorted(values)[0]
@@ -61,6 +38,7 @@ def prepare_dataset(dataset):
             except KeyError:
                     results[course][house] = []
                     results[course][house].append(grade)
+    # Here we normalize our grades
     for course, houses in results.items():
         for house, grades in houses.items():
             for i in range(len(grades)):
@@ -72,9 +50,8 @@ def calculate_variance(grades):
     grades_total, grades_mean = 0, 0
     grades_min, grades_max = get_min(grades), get_max(grades)
     for grade in grades:
-        normalized_grade = normalize(grade, grades_min, grades_max)
-        squared_grades_total += normalized_grade **2
-        grades_total += normalized_grade
+        squared_grades_total += grade **2
+        grades_total += grade
     squared_grades_mean = squared_grades_total / len(grades)
     grades_mean = grades_total / len(grades)
     return squared_grades_mean - (grades_mean ** 2)
@@ -89,47 +66,32 @@ def variance_per_course_per_house(dataset):
     for course in dataset.keys():
         for house in dataset[course].keys():
             results[course][house] = calculate_variance(dataset[course][house])
+    return results
 
-    results_bis = {}
-    for course in classes:
-        if course not in results.keys():
-            results[course] = {}
-    variances_all = []
-    for course in results.keys():
-        variances = []
-        for house in results[course].keys():
-            variances.append(results[course][house])
-        results_bis[course] = calculate_variance(variances)
-        variances_all.append(results_bis[course])
-    
-    print("Variance between students grades, per house per course")
-    for key, value in results.items():
-        print(key, value)
-
-    print('\n')
-    print("Variance between variance houses, per course")
-    for key, value in results_bis.items():
-        print(key, value)
-
-    grades_arrays = []
-    for grade in results_bis.values():
-        grades_arrays.append([grade])
-    plt.hist(grades_arrays, len(grades_arrays), density=True, histtype='bar', label=classes)
-    plt.legend(prop={'size':10})
-    plt.show()
-
-
+def plot_histogram(results_variance):
+    fig = go.Figure()
+    variance_per_house = {}
+    grades_per_house = {}
+    for course in results_variance.keys():
+        for house in results_variance[course]:
+            if house not in variance_per_house.keys():
+                variance_per_house[house] = []
+            variance_per_house[house].append(results_variance[course][house])
+    for house in variance_per_house.keys():
+        fig.add_trace(go.Histogram(histfunc="avg", x=list(results_variance.keys()), y=variance_per_house[house], name=house, marker_color=houses_colors[house], opacity=0.8))
+    fig.update_layout(title="Variances between the student's grades for each house and for each class (less variance means that the student's grades are homogeneous, more variance means that the student's grades are heterogeneous)", xaxis_title="Class", yaxis_title="Variance")
+    fig.show()
 
 def main():
-    result = {}
+    results = {}
     try:
         dataset = describe.retrieve_dataset('datasets/dataset_train.csv')
     except FileNotFoundError:
         print("File not found, exiting program")
         exit()
-    result = prepare_dataset(dataset)
-    # The results are now normalized
-    #variance_per_course_per_house(result)
+    results = prepare_dataset(dataset)
+    results_variance = variance_per_course_per_house(results)
+    plot_histogram(results_variance)
 
 if __name__ == "__main__":
     main()
