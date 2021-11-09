ddb2bc96e8c196da6f73496388d5cf0908db39e1
cchicote <cchicote@student.42.fr>
2021-11-10 00:51:36 +0100

    cleaned the repo a bit

--- old_stuff/histogram.py
+++ old_stuff/histogram.py
@@ -1,86 +1,29 @@
-import math
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
+import parse
 import describe
-from utils import *
 
-def prepare_dataset(dataset):
-    # The results data will have this format:
-    # {'Class1': {'House1': [grades], 'House2': [grades]}}
-    results = {}
-    grade_min, grade_max = {}, {}
-    for course in classes:
-        if course not in results.keys():
-            results[course] = {}
-    list_size = int(dataset['Index'][len(dataset['Index']) - 1])
-    for i in range(list_size + 1):
-        for course in classes:
-            try:
-                house = dataset['Hogwarts House'][i]
-                if house == '':
-                    continue
-                grade = describe.try_float(dataset[course][i])
-                if course not in grade_min.keys() or grade < grade_min[course]:
-                    grade_min[course] = grade
-                if course not in grade_max.keys() or grade > grade_max[course]:
-                    grade_max[course] = grade
-                results[course][house].append(grade)
-            except KeyError:
-                    results[course][house] = []
-                    results[course][house].append(grade)
-    # Here we normalize our grades
-    for course, houses in results.items():
-        for house, grades in houses.items():
-            for i in range(len(grades)):
-                results[course][house][i] = normalize(results[course][house][i], grade_min[course], grade_max[course])
-    return results
-
-def calculate_variance(grades):
-    squared_grades_total, squared_grades_mean = 0, 0
-    grades_total, grades_mean = 0, 0
-    grades_min, grades_max = get_min(grades), get_max(grades)
-    for grade in grades:
-        squared_grades_total += grade **2
-        grades_total += grade
-    squared_grades_mean = squared_grades_total / len(grades)
-    grades_mean = grades_total / len(grades)
-    return squared_grades_mean - (grades_mean ** 2)
-
-def variance_per_course_per_house(dataset):
-    # The results data will have this format:
-    # {'Class1': {'House1': variance, 'House2': variance}}
-    results = {}
-    for course in classes:
-        if course not in results.keys():
-            results[course] = {}
-    for course in dataset.keys():
-        for house in dataset[course].keys():
-            results[course][house] = calculate_variance(dataset[course][house])
-    return results
-
-def plot_histogram(results_variance):
+def my_histogram(df):
     fig = go.Figure()
-    variance_per_house = {}
-    for course in results_variance.keys():
-        for house in results_variance[course]:
-            if house not in variance_per_house.keys():
-                variance_per_house[house] = []
-            variance_per_house[house].append(results_variance[course][house])
-    for house in variance_per_house.keys():
-        fig.add_trace(go.Histogram(histfunc="avg", x=list(results_variance.keys()), y=variance_per_house[house], name=house, marker_color=houses_colors[house], opacity=0.8))
-    fig.update_layout(title="Variances between the student's grades for each house and for each class (less variance means that the student's grades are homogeneous, more variance means that the student's grades are heterogeneous)", xaxis_title="Class", yaxis_title="Variance")
+    for feature in df:
+        # Skip the Index column and the features that do not contain exclusively numeric values
+        if feature == "Index" or df[feature].dtype not in parse.numeric_values:
+            continue
+        count = describe.my_count(df[feature])
+        std = describe.my_std(df[feature], describe.my_mean(df[feature], count=count), ddof=1, count=count)
+        fig.add_trace(go.Histogram(histfunc="avg", x=[feature], y=[std], name=feature, opacity=0.8))
+
+    fig.update_layout(title="Standard deviation between the student's grades for each feature (less std means that the student's grades are homogeneous)", xaxis_title="Feature", yaxis_title="Standard deviation")
     fig.show()
 
 def main():
-    results = {}
-    try:
-        dataset = describe.retrieve_dataset('datasets/dataset_train.csv', fill_empty_nums=True)
-    except FileNotFoundError:
-        print("File not found, exiting program")
-        exit()
-    results = prepare_dataset(dataset)
-    results_variance = variance_per_course_per_house(results)
-    plot_histogram(results_variance)
+    # Read CSV file with pandas
+    df_orig = parse.read_file("datasets/dataset_train.csv")
+
+    # Normalize the values of the dataframe
+    df = parse.normalize_df(df_orig.copy())
+
+    # Calculate the standard deviation for each feature and plot it
+    my_histogram(df)
 
 if __name__ == "__main__":
     main()
