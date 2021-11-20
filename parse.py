0c14f84ac3be55fe30865f224481b759e340ac4a
cchicote <cchicote@student.42.fr>
2021-11-20 14:40:00 +0100

    Testing csv file validity

--- parse.py
+++ parse.py
@@ -5,11 +5,27 @@ import constants as cst
 import argparse
 from pathlib import Path
 
-def get_filename(av, ac):
-	if ac != 2:
-		print("usage: ./describe.py [dataset]")
-		exit()
-	return av[1]
+def check_data_integrity(df, check_hog=True):
+    required_col = ["Index", "Hogwarts House"]
+    required_col.extend(cst.feat_list)
+    max_nan_p = 0.75
+    # Check if dataset is empty
+    if df.empty:
+        print("ERROR: Empty dataset")
+        return None
+    # Check if the required columns are present, not empty, and contain less than max_nan_p% nan values
+    for col in required_col:
+        if col not in df.columns:
+            print(f"ERROR: missing column {col}")
+            return None
+        if (not len(df[col])):
+            print(f"ERROR: empty {col} column")
+        if not check_hog and col == "Hogwarts House":
+            continue
+        if (df.isna().sum()[col] / len(df[col]) > max_nan_p):
+            print("ERROR: %s column contains too many nan values (more than %d%%)" % (col, max_nan_p * 100))
+            return None
+    return df
 
 def check_valid_file(fname, parser, suffix, check_path):
     # If file extension is not valid: print help and return error
@@ -87,7 +103,7 @@ def normalize_df(df):
 #def normalize_df(df):
 #    return df.apply(lambda x: (x - describe.my_min(x)) / (describe.my_max(x) - describe.my_min(x)) if (describe.my_min(x) != describe.my_max(x)) else 0)
 
-def read_file(filename):
+def read_file(filename, check_hog=True):
     try:
         df = pd.read_csv(filename)
     except FileNotFoundError:
@@ -97,5 +113,10 @@ def read_file(filename):
     except pd.errors.ParserError:
         print("Invalid file, exiting program")
     else:
+        if df is None:
+            print("Read csv returned None, exiting program")
+            exit(1)
+        if check_data_integrity(df, check_hog) is None:
+            exit(1)
         return df
     exit(1)
